b265ac858f2a610b865975a3b5c3b49e09188076
Raymond Feng <raymond@strongloop.com>
2014-03-10 16:37:39 -0700

    Clean up the example

--- example/stock-ws.js
+++ example/stock-ws.js
@@ -8,9 +8,13 @@ var ds = loopback.createDataSource('soap',
 
   });
 
+// Create the model
 var StockQuote = ds.createModel('StockQuote', {});
 
+// Unfortunately, the methods from the connector are mixed in asynchronously
+// This is a hack to wait for the methods to be injected
 setTimeout(function () {
-  console.log(Object.keys(StockQuote));
-  StockQuote.GetQuote({symbol: 'IBM'}, console.log);
+  StockQuote.GetQuote({symbol: 'IBM'}, function(err, response) {
+    console.log('Response: ', response);
+  });
 }, 500);
