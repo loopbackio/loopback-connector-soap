744ec5ef76785822e927d5a9554eead35520e5dd
Raymond Feng <raymond@strongloop.com>
2014-03-11 09:33:23 -0700

    Use 'connected' event to create the model

--- example/stock-ws.js
+++ example/stock-ws.js
@@ -8,13 +8,14 @@ var ds = loopback.createDataSource('soap',
 
   });
 
-// Create the model
-var StockQuote = ds.createModel('StockQuote', {});
-
 // Unfortunately, the methods from the connector are mixed in asynchronously
 // This is a hack to wait for the methods to be injected
-setTimeout(function () {
-  StockQuote.GetQuote({symbol: 'IBM'}, function(err, response) {
+ds.once('connected', function () {
+// Create the model
+  var StockQuote = ds.createModel('StockQuote', {});
+
+  StockQuote.GetQuote({symbol: 'IBM'}, function (err, response) {
     console.log('Response: ', response);
   });
-}, 500);
+
+});
