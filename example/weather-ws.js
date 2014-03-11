32a53c6b23c7c028751afea71e517243dbeaced9
Raymond Feng <raymond@strongloop.com>
2014-03-11 10:25:13 -0700

    Add a weather example

---
+++ example/weather-ws.js
@@ -0,0 +1,24 @@
+var loopback = require('loopback');
+
+var ds = loopback.createDataSource('soap',
+  {
+    connector: require('../index'),
+    wsdl: 'http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL' // The url to WSDL
+
+  });
+
+// Unfortunately, the methods from the connector are mixed in asynchronously
+// This is a hack to wait for the methods to be injected
+ds.once('connected', function () {
+// Create the model
+  var WeatherService = ds.createModel('WeatherService', {});
+
+  WeatherService.GetCityForecastByZIP({ZIP: '94555'}, function (err, response) {
+    console.log('Forecast: %j', response);
+  });
+
+  WeatherService.GetCityWeatherByZIP({ZIP: '94555'}, function (err, response) {
+    console.log('Weather: %j', response);
+  });
+
+});
