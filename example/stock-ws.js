35e5cbcb0173c8648d845c94aaad00b197ba04b5
Raymond Feng <raymond@strongloop.com>
2014-03-14 16:31:51 -0700

    Add support for custom method mappings

--- example/stock-ws.js
+++ example/stock-ws.js
@@ -4,8 +4,23 @@ var ds = loopback.createDataSource('soap',
   {
     connector: require('../index'),
     url: 'http://www.webservicex.net/stockquote.asmx?WSDL', // The url to WSDL
-    endpoint: 'http://www.webservicex.net/stockquote.asmx' // The service endpoint
+    endpoint: 'http://www.webservicex.net/stockquote.asmx', // The service endpoint
 
+    // Map SOAP service/port/operation to Node.js methods
+    operations: {
+      // The key is the method name
+      stockQuote: {
+        service: 'StockQuote', // The WSDL service name
+        port: 'StockQuoteSoap', // The WSDL port name
+        operation: 'GetQuote' // The WSDL operation name
+      },
+      // The key is the method name
+      stockQuote12: {
+        service: 'StockQuote', // The WSDL service name
+        port: 'StockQuoteSoap12', // The WSDL port name
+        operation: 'GetQuote' // The WSDL operation name
+      }
+    }
   });
 
 // Unfortunately, the methods from the connector are mixed in asynchronously
@@ -14,8 +29,13 @@ ds.once('connected', function () {
 // Create the model
   var StockQuote = ds.createModel('StockQuote', {});
 
-  StockQuote.GetQuote({symbol: 'IBM'}, function (err, response) {
+  StockQuote.stockQuote({symbol: 'IBM'}, function (err, response) {
     console.log('Response: ', response);
   });
+
+  StockQuote.stockQuote12({symbol: 'FB'}, function (err, response) {
+    console.log('Response: ', response);
+  });
+
 
 });
